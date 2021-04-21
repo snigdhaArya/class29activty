d9f419d8a6b527f64957128f06a56526cc4dfd76
snigdha <snigdha4128@gmail.com>
2021-04-21 18:00:02 +0530

    slingshot added

--- Slingshot.js
+++ Slingshot.js
@@ -7,6 +7,10 @@ class SlingShot{
             length: 10
         }
         this.pointB = pointB
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
+
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,12 +20,35 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
+            if(pointA.x<200){
+       
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(71, 22, 22)
+
+            line(pointA.x-25 , pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-25 , pointA.y, pointB.x+30, pointB.y);
+            image(this.sling3,pointA.x-30,pointA.y-10,15,30);
         }
+        else{
+            
+            strokeWeight(4);
+            stroke(71, 22, 22)
+
+            line(pointA.x+25 , pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25 , pointA.y, pointB.x+30, pointB.y);
+            image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+        }
+            pop();
+        }
+                 
+
     }
     
 }
